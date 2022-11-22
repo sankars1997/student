d88047051ef10e4d37eddec3b28ca7bbb2c6f6ca
sankar <sankarsuresh1997@gmail.com>
2022-11-22 08:51:50 +0530

    student

---
+++ student.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class student {
+
+    public static void main(String[] args) {
+        int op;
+        ArrayList<student> arr = new ArrayList<student>();
+
+        System.out.println("Enter the option:");
+        System.out.println("--------------------------------------------");
+        System.out.println("1.Add\n2.View\n3.Search\n4.Delete\n5.Exit");
+        Scanner sc = new Scanner(System.in);
+        op = sc.nextInt();
+
+    }
+}
